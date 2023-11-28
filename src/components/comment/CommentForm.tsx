a04dcbda8d38787d11439e2b5e2053d43c801c97
Woohyeok Baek <qordngur156@gmail.com>
2023-11-28 17:26:25 +0900

    Feat : 댓글작성 기능개발

--- src/components/comment/CommentForm.tsx
+++ src/components/comment/CommentForm.tsx
@@ -1,16 +1,70 @@
 import styles from './Comment.module.scss'
+import { useContext, useState } from 'react'
+import AuthContext from 'context/AuthContext'
+import { arrayUnion, doc, updateDoc } from 'firebase/firestore'
+import { db } from 'firebaseApp'
+// 데이터타입
+import { PostType } from 'interface'
 
-export default function CommentForm() {
 
+interface CommentFormProps {
+    post : PostType,
+}
+
+export default function CommentForm({ post } : CommentFormProps) {
+    const { user } = useContext(AuthContext)
+    const [ content, setContent ] = useState<string>('')
+
+    // submit 핸들러
+    const handleSubmit = async (e : React.FormEvent<HTMLFormElement>) => {
+        e?.preventDefault()
+
+        try {
+            const postRef = doc(db, 'posts', post?.id)
+            const insertComment = {
+                uid : user?.uid,
+                displayName : user?.displayName,
+                email : user?.email,
+                content : content,
+                createdAt : new Date().toLocaleDateString("ko", {
+                    hour : '2-digit',
+                    minute : '2-digit',
+                    second : '2-digit'
+                }), 
+            }
+
+            // 게시글 comments필드에 댓글추가
+            await updateDoc(postRef, {
+                comments : arrayUnion(insertComment)
+            })
+            setContent('') // 댓글작성후, content초기화
+            console.log('덧글을 작성하였습니다.')
+
+        } catch(err : any) {
+            console.log(err?.code)
+        }
+    }
+
+    // content 핸들러
+    const handleContentChange = (e : React.ChangeEvent<HTMLTextAreaElement>) => {
+        const { value } = e?.target;
+        setContent(value)
+    }
+
+    
     return (
-        <form className={ styles.commentForm }>
+        <form onSubmit={ handleSubmit } className={ styles.commentForm }>
             <textarea 
                 className={ styles.commentForm__textarea }
-                placeholder='덧글을 입력하십시오.'
+                onChange={ handleContentChange }
+                value={ content }
+                placeholder={ user?.uid ? '덧글을 입력하십시오.' : '접속이후 이용해주십시오.' }
+                disabled={ !user?.uid }
                 spellCheck={false}
             />
             <div className={ styles.commentForm__flexReverse }>
-                <input type='submit' value="덧글 남기기" className={ styles.commentForm__inputBtn }/>
+                <input type='submit' value="덧글 남기기" className={ styles.commentForm__inputBtn }
+                    disabled={!(user?.uid && content)}/>
             </div>
         </form>
     )
--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -91,6 +91,7 @@ export default function PostPage() {
                 <h1>게시물</h1>
             </div>
 
+            { post?.id && <> 
             <div className="post">
                 <div className="post__user-img"></div>
 
@@ -131,55 +132,10 @@ export default function PostPage() {
                     <CommentItem/>
                     <CommentItem/>
                 </div>
-                <CommentForm/>
-            </div>
+                <CommentForm post={ post }/>
+            </div> 
+            </> }
+
         </div>
     )
-
-    // return (
-    //     <div className="page">
-    //         <div className="page__header">
-    //             <h1>게시물</h1>
-    //         </div>
-
-    //         <div className="post">
-    //             <div className="post__header">
-    //                 <div className="post__user-img"></div>
-    //                 <div>
-    //                     <div className="post__name">글쓴이 : </div>
-    //                     <div className="post__created">날짜 : </div>
-    //                 </div>
-    //             </div>
-
-    //             <div className="post__content">
-    //                 <div className="post__text">하이요</div>
-    //             </div>
-
-    //             <div className="post__footer">
-    //                 <div className="post__flex">
-    //                     <div className="post__like">
-    //                         추천 : 0
-    //                     </div>
-    //                     <div>덧글 : 0</div>
-    //                 </div>
-    //                 <div className="post__flex">
-    //                     <div className="post__edit">
-    //                         <Link to={`/post/edit/${1}`}>편집</Link>
-    //                     </div>
-    //                     <div className="post__delete">삭제</div>
-    //                 </div> 
-    //             </div>
-    //         </div>
-
-    //         <div>
-    //             <div>
-    //                 <CommentItem/>
-    //                 <CommentItem/>
-    //                 <CommentItem/>
-    //             </div>
-    //             <CommentForm/>
-    //         </div>
-            
-    //     </div>
-    // )
 }
