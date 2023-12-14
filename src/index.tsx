9e29eba5dca81eb51f21f11162133c270e84dba9
Woohyeok Baek <qordngur156@gmail.com>
2023-12-14 16:59:30 +0900

    sass 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from 'context/AuthContext';
