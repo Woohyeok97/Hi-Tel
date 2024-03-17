5077a5cecf0783b5b20894f135c7a8264e6bc173
Woohyeok Baek <qordngur156@gmail.com>
2024-03-17 14:25:04 +0900

    Chore: emotion 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
 import './tailwind.css'
-import { useContext } from "react";
-import AuthContext from "context/AuthContext";
-import { RecoilRoot } from "recoil";
+import { useContext } from 'react';
+import AuthContext from 'context/AuthContext';
+import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import globalStyles from 'styles/globalStyles';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 // components
+import { Global } from '@emotion/react';
 import Router from "Router";
 import Layout from "components/layout/Layout";
 import Loader from 'components/UI/Loader';
 
-const queryClient = new QueryClient()
 
-
+const queryClient = new QueryClient();
 
 function App() {
-    const { user, init } = useContext(AuthContext)
-  
-    return (
-        <RecoilRoot> 
-            <QueryClientProvider client={ queryClient }>
-                <Layout>
-                { init && <Router isAuthentication={ !!user }/> }
-                </Layout>
-                <ReactQueryDevtools/>
-            </QueryClientProvider>
-        </RecoilRoot>
-    );
+  const { user, init } = useContext(AuthContext);
+
+  return (
+    <>
+    <Global styles={globalStyles} />
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot> 
+        <Layout>
+        {init && <Router isAuthentication={!!user} /> }
+        </Layout>
+        <ReactQueryDevtools/>
+      </RecoilRoot>
+    </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,26 @@
+import { css } from '@emotion/react';
+
+export default css`
+  body {
+    color: #ffffff;
+    height: 100vh;
+  }
+  input {
+    outline: none;
+    border: none;
+    box-sizing: border-box;
+  }
+  textarea {
+    outline: none;
+    border: none;
+    box-sizing: border-box;
+    resize: none;
+    }
+  h1 {
+    margin: 0;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`
